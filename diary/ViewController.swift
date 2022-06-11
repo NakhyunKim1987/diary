7a997a1c59667e76dfa31db5385592fe8755c8c3
nakhyun.kim <kkokkokim15@gmail.com>
2022-06-12 02:01:51 +0900

    Initial Commit

---
+++ diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diary
+//
+//  Created by NakHyun Kim on 2022/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
