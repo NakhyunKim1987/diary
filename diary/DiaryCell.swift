d7b5cfb08d4b10b40bc033aa430679b065ba0592
nakhyun.kim <kkokkokim15@gmail.com>
2022-06-12 02:17:07 +0900

    first commit

---
+++ diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  diary
+//
+//  Created by NakHyun Kim on 2022/06/12.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  diary
+//
+//  Created by NakHyun Kim on 2022/06/12.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
